9817e877518b6ec68cd5a5a0a397ef81720abc16
helvaciBelemir <helvacibelemir@gmail.com>
2024-01-23 10:13:51 -0800

    23.01.2024  belemir helvaci

---
+++ src/HataAyıklama.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class HataAyıklama {
+
+
+    public static void DosyaOkuma() throws FileNotFoundException {
+        try {
+
+            Bolme(Topla(5, 8),0);
+        } catch (final Exception e) {
+            System.out.println("Bolme islemi yapilamadi" +e.getStackTrace());
+            return;
+        }
+
+    }
+    public static int Topla(int x,int y){
+
+            int sum=x+y;
+
+            return sum;
+        }
+
+    public static int Bolme(int x,int y){
+
+        int result=x/y;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            DosyaOkuma();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
